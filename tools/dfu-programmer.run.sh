ec5f5a3ed8eccf4fd418aa5d5d0fd1efd49ea500
flabbergast <s3+flabbergast@sdfeu.org>
2015-03-29 21:42:33 +0100

    Add dfu-programmer binary for OS X.

--- tools/dfu-programmer.run.sh
+++ tools/dfu-programmer.run.sh
@@ -1,7 +1,21 @@
 #!/bin/bash -x
-#echo RR$1RR RR$2RR RR$3RR RR$4RR RR$5RR RR$6RR RR$7RR
-PATH=$PATH:/usr/local/bin
-dfu-programmer $1 erase
-dfu-programmer $@
-dfu-programmer $1 reset
+# determine the dir where we reside
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# determine platform/arch
+PLATFORM=`uname`
+ARCH=`uname -m`
+#PATH=$PATH:/usr/local/bin
+DFU_COMMAND=dfu-programmer
+if [[ $PLATFORM == "Darwin" ]]; then
+  DFU_COMMAND="$DIR/dfu-programmer.osx"
+elif [[ $PLATFORM == "Linux" ]]; then
+  if [[ $ARCH == "x86_64" ]]; then
+    DFU_COMMAND="$DIR/dfu-programmer.linux.x86_64"
+  elif [[ $ARCH == "i386" ]]; then
+    DFU_COMMAND="$DIR/dfu-programmer.linux.i386"
+  fi
+fi
+"$DFU_COMMAND" $1 erase
+"$DFU_COMMAND" $@
+"$DFU_COMMAND" $1 reset
 exit 0
